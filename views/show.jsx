5a6f883a2721b148468bfb73a51643dc867943bd
User <j_camacho8@yahoo.com>
2022-09-26 20:43:59 -0700

    edit and update route complete

--- views/show.jsx
+++ views/show.jsx
@@ -1,25 +1,32 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Show ({bread}) {
+function Show({ bread, index }) {
   console.log("bread name:", bread.name)
-    return (
-        <Default>
-        <h3>{bread.name}</h3>
-        <p>
-          and it
-          {
-            bread.hasGluten
+  return (
+    <Default>
+      <h3>{bread.name}</h3>
+      <p>
+        and it
+        {
+          bread.hasGluten
             ? <span> does </span>
             : <span> does NOT </span>
-          }
-          have gluten.
-        </p>
-        <img src={bread.image} alt={bread.name} />
-        <li><a href="/breads">Go home</a></li>
-      </Default>
-      
-    )
+        }
+        have gluten.
+      </p>
+      <img src={bread.image} alt={bread.name} />
+      <li><a href="/breads">Go home</a></li>
+
+      <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
+      <form action={`/breads/${index}?_method=DELETE`} method="POST">
+        <input type='submit' value="DELETE" />
+      </form>
+
+    </Default>
+
+  )
 }
 
 module.exports = Show
